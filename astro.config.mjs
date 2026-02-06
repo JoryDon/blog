f05d88c80cbca91c950a93ffc2bedab47338ee55
donglei <donglei@efreight.cn>
2026-02-06 12:36:43 +0800

    修复报错

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,19 @@ import db from '@astrojs/db';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+import expressive from 'astro-expressive-code';
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [UnoCSS(), mdx(), sitemap(), db()],
+  integrations: [UnoCSS(), expressive(), mdx(), sitemap(), db()],
 
   vite: {
     plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ['astro-expressive-code']
+    },
   },
 });
